c23d468439717efed1f056f9f46824a3e7dfe10d
Armando <rivera.armando997@gmail.com>
2020-03-18 18:44:17 -0600

    Componentes sidebar, slider, header y footer

--- LearningAngular/src/app/app.module.ts
+++ LearningAngular/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 // Importamos mi-componente
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     // Incluimos el componente recién importado para poder utilizarlo.
     MiComponente,
-    PeliculasComponent
+    PeliculasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
