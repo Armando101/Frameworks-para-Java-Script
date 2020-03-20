fb1e874a31007638b08b11f00c55e9b595bf285d
Armando <rivera.armando997@gmail.com>
2020-03-20 14:55:33 -0600

    Agregando imagenes con directivas *ngIf y *ngFor

--- LearningAngular/src/app/components/peliculas/peliculas.component.ts
+++ LearningAngular/src/app/components/peliculas/peliculas.component.ts
@@ -8,12 +8,18 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 export class PeliculasComponent implements OnInit {
 
 	public titulo: string;
+  public peliculas: Array<any>;
 
 	// En el constructor se asignan valores a las propiedades
 	// No es buena práctica agregar lógica aquí, eso va en el OnInit
-  constructor() { 
+  constructor() {
   	this.titulo = 'Componente película';
-  	console.log('Constructor lanzado');
+    this.peliculas = [
+      {title: "Requiem for a dream", image: 'https://images-na.ssl-images-amazon.com/images/I/81OOo4oFkcL._SL1500_.jpg'},
+      {title: "Harry Potter y el prisionero de Azcaban", image: 'https://es.web.img2.acsta.net/pictures/14/04/30/11/36/185120.jpg'},
+      {title: "Psicosis", image: 'https://diariodefriki.files.wordpress.com/2017/10/psycho-559c10640aba2.jpg?w=210&h=300'}
+    ];
+    // console.log('Constructor lanzado');
   }
 
   // OnInit es un hook que se ejecuta cuando cargamos la etiqueta
