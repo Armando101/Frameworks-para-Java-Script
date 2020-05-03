b9dafdc0f5c1318e8c1de37d3a16caebd41164e9
Armando <rivera.armando997@gmail.com>
2020-05-03 01:24:31 -0500

    Angular -Peticiones http para consumir nuestra API y mostrar los artículos en consola. Creamos un servicio de Artículos

--- LearningAngular/src/app/components/blog/blog.component.ts
+++ LearningAngular/src/app/components/blog/blog.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
+  providers: [ArticleService]
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  	private _articleService: ArticleService
+  ) { }
 
   ngOnInit(): void {
+  	// this._articleService.pruebas();
+
+  	// subscribe me permite recoger los datos que me devuelve la petición http
+  	// Dentro de subscribe tengo dos funciones de callback
+  	// Uno para recibir la respuesta y otro para el error
+  	this._articleService.getArticles()
+  	.subscribe(
+  		response => {
+  			console.log(response);
+  		},
+  		error => {
+  			console.error(error);
+  		}
+  	);
   }
 
 }
---
+++ LearningAngular/src/app/services/article.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core'; // Para crear mi servicio
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Para hacer peticiones HTTP
+import { Observable } from 'rxjs'; // rxjs librería de JS para crear observables
+import { Article } from '../models/article';
+import { Global } from './global';
+
+// Importante incluir el HttpClientModule en el app.models
+
+@Injectable()
+export class ArticleService {
+
+	public url: string;
+
+	constructor(
+		private _http: HttpClient
+	) {
+		this.url = Global.url;
+	}
+
+	pruebas() {
+		return 'Soy el servicio de artículos!!!'
+	}
+
+	// Indico que va a devolver un observable
+	// Un Onservable me permite recoger los datos que me devuelven los métodos de los servicios
+	getArticles():Observable<any> {
+		return this._http.get(this.url + 'articles');
+	}
+}
