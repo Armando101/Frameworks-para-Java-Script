82ed2647621d5891fb0745d95126c4053573af7e
Armando <rivera.armando997@gmail.com>
2020-05-02 22:35:15 -0500

    Angular -Pipes personalizados y formularios

--- LearningAngular/src/app/app.module.ts
+++ LearningAngular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 // Importamos mi-componente
@@ -37,8 +38,10 @@ import { EsParPipe } from './pipes/espar.pipe';
     EsParPipe
   ],
   imports: [
+    // Aquí van los módulos
     BrowserModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
