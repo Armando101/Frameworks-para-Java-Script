03c045f82eccc1883de14a79aa0fb0197ef99bff
Armando <rivera.armando997@gmail.com>
2020-05-17 16:25:54 -0500

    Angular Mostrando un articulo individual y botones de editar y eliminar

--- LearningAngular/src/app/components/article/article.component.ts
+++ LearningAngular/src/app/components/article/article.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article';
+
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  providers: [ArticleService]
 })
 export class ArticleComponent implements OnInit {
 
-  constructor() { }
+	public article: Article;
+	public url: string;
+
+  constructor(
+  	private _articleService: ArticleService,
+  	private _route: ActivatedRoute,
+  	private _router: Router,
+  ) {
+  	this.url = Global.url;
+  }
 
   ngOnInit(): void {
+  	this._route.params.subscribe(params => {
+  		// console.log(params);	// Objeto con el id únicamente
+  		let id = params['id'];
+  		this._articleService.getArticle(id).subscribe(
+  		response => {
+  			if (response.article) {
+  				this.article = response.article;
+  			} else {
+  				this._router.navigate(['/home']);
+  			}
+  			// console.log(response);		// Objeto con los datos del artículo 
+  		}, 
+  		error => {
+  			console.log(error);
+  			this._router.navigate(['/home']);
+  		});
+  	});
   }
 
 }
