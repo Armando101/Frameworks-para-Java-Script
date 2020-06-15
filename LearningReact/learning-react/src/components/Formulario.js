54779172c73e34e8d4c27d461e2d50dc27f47921
Armando <rivera.armando997@gmail.com>
2020-06-14 20:06:48 -0500

    Learning React -Profundizando en Formularios

--- LearningReact/learning-react/src/components/Formulario.js
+++ LearningReact/learning-react/src/components/Formulario.js
@@ -11,11 +11,39 @@ class Formulario extends Component {
 	generoMujerRef = React.createRef();
 	generoOtroRef = React.createRef();
 
+	state = {
+		user: {}
+	};
+
 	recibirFormulario = (event) => {
 		event.preventDefault(); // Esto es para que no recargue la pagina
-		console.log('Formulario enviado');
-		console.log(this.nombreRef);
-		console.log(this.nombreRef.current.value);
+		
+		let genero;
+		if (this.generoHombreRef.current.checked) {
+			genero = this.generoHombreRef.current.value;
+		} else if (this.generoMujerRef.current.checked) {
+			genero = this.generoMujerRef.current.value
+		} else {
+			genero = this.generoOtroRef.current.value
+		}
+
+		console.log(genero);
+
+		const user = {
+			name: this.nombreRef.current.value,
+			lastname: this.apellidosRef.current.value,
+			bio: this.bioRef.current.value,
+			gender: genero
+		}
+
+		this.setState({
+			user: user
+		});
+
+		// console.log('Formulario enviado');
+		// console.log(this.nombreRef);
+		// console.log(this.nombreRef.current.value);
+		console.log(user);
 	}
 
 	render() {
@@ -24,7 +52,16 @@ class Formulario extends Component {
 			    <div className="center">
 					<div id="content">
 								<h1 className="subheader">Formulario</h1>
-					      <form className="mid-form" onSubmit={this.recibirFormulario}>
+					      {/*Mostar datos del formulario*/}
+					      {this.state.user.name &&
+					      	<div id='user-data'>
+					      		<p>Nombre: <strong>{this.state.user.name}</strong></p>
+					      		<p>Apellido: <strong>{this.state.user.lastname}</strong></p>
+					      		<p>Biografia: <strong>{this.state.user.bio}</strong></p>
+					      		<p>Gender: <strong>{this.state.user.gender}</strong></p>
+					      	</div>
+					      }
+					      <form className="mid-form" onSubmit={this.recibirFormulario} onChange={this.recibirFormulario}>
 					        <div className="form-group">
 					          <label htmlFor="nombre">Nombre</label>
 					          <input type="text" name="nombre" ref={this.nombreRef}/>
@@ -41,8 +78,8 @@ class Formulario extends Component {
 					        </div>
 					        <div className="form-group radio-buttons">
 					          <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
-					          <input type="radio" name="genero" value="hombre"  ref={this.generoMujerRef}/> Mujer
-					          <input type="radio" name="genero" value="hombre"  ref={this.generoOtroRef}/> Otro
+					          <input type="radio" name="genero" value="mujer"  ref={this.generoMujerRef}/> Mujer
+					          <input type="radio" name="genero" value="otro"  ref={this.generoOtroRef}/> Otro
 					        </div>
 					        <div className="clearfix"></div>
 					        <input className="btn btn-succes" type="submit" name="enviar" value="Enviar"/>
