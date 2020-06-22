9992407298f9d4e9017c6fa7f220a1a5998927e0
Armando <rivera.armando997@gmail.com>
2020-06-22 04:11:06 -0500

    IonicApp -Alert para agregar nuevas listas y guardarlas en el localStorage

--- AngularProjects/04-toDo/src/app/services/deseos.service.ts
+++ AngularProjects/04-toDo/src/app/services/deseos.service.ts
@@ -10,13 +10,33 @@ export class DeseosService {
 	private listas: Lista[] = [];
 
   constructor() {
-  	const lista1 = new Lista('Aprender diferentes idiomas');
+  	this.cargarStorage();
+
+  	/*const lista1 = new Lista('Aprender diferentes idiomas');
   	const lista2 = new Lista('Sobrevivir al 2020');
 
-  	this.listas.push(lista1, lista2);
+  	this.listas.push(lista1, lista2);*/
   }
 
   getListas(): Lista[] {
   	return this.listas;
+  }
+
+  crearLista(title: string) {
+  	const nuevaLista = new Lista(title);
+  	this.listas.push(nuevaLista);
+  	this.guardarStorage();
+  }
+
+  guardarStorage() {
+  	localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+  	if (localStorage.getItem('data')) {
+  		this.listas = JSON.parse(localStorage.getItem('data'));
+  	} else {
+  		this.listas = [];
+  	}
   }
 }
