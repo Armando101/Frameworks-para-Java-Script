8712b9868ad77179acf7268c09d382bd4d00852f
Armando <rivera.armando997@gmail.com>
2020-06-22 03:44:41 -0500

    IonicApp -Tab de agregar, checkbox, input item y botón para ir a la página principal

--- AngularProjects/04-toDo/src/app/pages/tab1/tab1.page.ts
+++ AngularProjects/04-toDo/src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models/lista.model';
@@ -13,10 +14,14 @@ export class Tab1Page {
 	public listas: Lista[];
 
   constructor(
-  	private deseosService: DeseosService
+  	private deseosService: DeseosService,
+  	private router: Router
   ) {
   	this.listas = this.deseosService.getListas();
   }
 
+  agregarLista() {
+  	this.router.navigateByUrl('/tabs/tab1/agregar');
+  }
 
 }
