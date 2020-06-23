68857380a274560dd346de5fd69c3eaf5f41c536
Armando <rivera.armando997@gmail.com>
2020-06-23 02:20:54 -0500

    Auth0App -Implementando un Guard para proteger una pagina

--- AngularProjects/06-authApp/src/app/app-routing.module.ts
+++ AngularProjects/06-authApp/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './services/auth.guard';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
@@ -9,7 +11,7 @@ import { PreciosComponent } from './components/precios/precios.component';
 const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'precios', component: PreciosComponent },
-	{ path: 'protegida', component: ProtegidaComponent },
+	{ path: 'protegida', component: ProtegidaComponent, canActivate: [ AuthGuard ] },
 	{ path: '**', pathMatch: 'full', redirectTo:'home' },
 
 ];
