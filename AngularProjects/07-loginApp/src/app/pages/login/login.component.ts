290a66247c0bd0543d2cfc0b3feb4820954ae411
Armando <rivera.armando997@gmail.com>
2020-06-24 14:59:05 -0500

    LoginApp -Login de usuarios

--- AngularProjects/07-loginApp/src/app/pages/login/login.component.ts
+++ AngularProjects/07-loginApp/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,9 @@ export class LoginComponent implements OnInit {
 
 	private usuario: UsuarioModel;
 
-  constructor() { }
+  constructor(
+  	private auth: AuthService
+  ) { }
 
   ngOnInit() {
   	this.usuario = new UsuarioModel();
@@ -21,7 +24,29 @@ export class LoginComponent implements OnInit {
   	if (form.invalid) {
 		return;
 	}  	
-  	console.log('Formulario válido');
+  	// console.log('Formulario válido');
+  	
+  	this.auth.login(this.usuario)
+  		.subscribe(response => {
+  			/*
+  				Ejemplo de respuesta
+
+  				{
+				  "localId": "ZY1rJK0eYLg...",
+				  "email": "[user@example.com]",
+				  "displayName": "",
+				  "idToken": "[ID_TOKEN]",
+				  "registered": true,
+				  "refreshToken": "[REFRESH_TOKEN]",
+				  "expiresIn": "3600"
+				}
+				
+				Docs: https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-password
+  			*/
+  			console.log(response);
+  		}, (err) => {
+  			console.log(err.error.error.message);
+  		});
   }
 
 }
