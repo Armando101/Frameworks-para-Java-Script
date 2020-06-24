6d1d9220eccff356f63f0f931b1199b9d5754f80
Armando <rivera.armando997@gmail.com>
2020-06-23 19:16:02 -0500

    Miscelenos -Recibiendo parametros de la ruta padre en el la ruta hija

--- AngularProjects/05-miscelaneus/src/app/components/usuario/usuario-nuevo.component.ts
+++ AngularProjects/05-miscelaneus/src/app/components/usuario/usuario-nuevo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-nuevo',
@@ -11,7 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuarioNuevoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: ActivatedRoute
+  ) {
+    /*Recogemos los parametros que vienen el la ruta del padre*/
+    this.router.parent.params.subscribe(parametros => {
+      console.log('Ruta hija nuevo');
+      console.log(parametros);
+    });
+  }
 
   ngOnInit(): void {
   }
