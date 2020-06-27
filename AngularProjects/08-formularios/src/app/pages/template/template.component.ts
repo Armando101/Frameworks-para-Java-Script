92dd953f0f52e2f2d21f3224b05f4ae1f123ca05
Armando <rivera.armando997@gmail.com>
2020-06-27 01:16:35 -0500

    Formularios -Validando formularios y mostrando mensajes de error

--- AngularProjects/08-formularios/src/app/pages/template/template.component.ts
+++ AngularProjects/08-formularios/src/app/pages/template/template.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+	public usuario = {
+		nombre: 'Armando',
+		apellido: 'Riverea',
+		correo: 'armando@rivera.com'
+
+		/*	
+		nombre: '',
+		apellido: '',
+		correo: ''
+		*/
+	}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form:NgForm) {
+  	// console.log("Submit disparado");
+  	// console.log(form);
+
+  	// El siguiente if me permite mostrar menesaje de error
+  	// dependiendo el campo que haga falta en el formulario
+  	if (form.invalid) {
+  		Object.values(form.controls).map(control => {
+  			if (control.status === "INVALID") {
+	  			// console.log(control);
+	  			control.markAsTouched();
+  			}
+  		});
+  		return;
+  	}
+
+  	console.log(form.value);
   }
 
 }
