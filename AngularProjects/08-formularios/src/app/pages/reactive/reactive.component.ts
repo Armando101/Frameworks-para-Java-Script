dac17e4a6b97e3142d3ee7ac5d71f0e3b6ff046b
Armando <rivera.armando997@gmail.com>
2020-06-27 02:43:01 -0500

    Formularios -Validando formulario reactivo

--- AngularProjects/08-formularios/src/app/pages/reactive/reactive.component.ts
+++ AngularProjects/08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -19,17 +19,39 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  get nombreNoValido() {
+  	return this.form.get('nombre').invalid && this.form.get('nombre').touched
+  }
+
+  get apellidoNoValido() {
+  	return this.form.get('apellido').invalid && this.form.get('apellido').touched
+  }
+
+  get correoNoValido() {
+  	return this.form.get('correo').invalid && this.form.get('correo').touched
+  }
+
   crearFormulario() {
   	this.form = this.fb.group({
   		// Definimos propiedades
   		// [Valor por defecto, validaciones sincoronos, validaciones asincronas]
-  		nombre: ['Armando'],
-  		apellido: ['Rivera'],
-  		correo: ['armando@rivera.com'],
+  		nombre: ['', [Validators.required, Validators.minLength(5)]],
+  		apellido: ['', Validators.required],
+  		correo: ['', [Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
   	});
   }
 
   guardar() {
+  	if (this.form.invalid) {
+  		Object.values(this.form.controls).map(control => {
+  			if (control.status === "INVALID") {
+	  			// console.log(control);
+	  			control.markAsTouched();
+  			}
+  		});
+  		return;
+  	}
+
   	// console.log(this.form);
   	console.log(this.form.value);
   }
