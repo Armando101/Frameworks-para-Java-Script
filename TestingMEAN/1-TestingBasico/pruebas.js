0d4c1c004beb29e94d0a208eab26f23a9ad41b07
Armando <rivera.armando997@gmail.com>
2020-06-29 02:37:48 -0500

    TestingMEAN -Expect and It

--- TestingMEAN/1-TestingBasico/pruebas.js
+++ TestingMEAN/1-TestingBasico/pruebas.js
@@ -5,5 +5,33 @@ console.log(saludar('Armando'));
 const resuldado = saludar('Armando');
 const esperado = 'Hola Armando';
 
-if (resuldado === esperado) { console.log('Prueba exitosa') }
-else { console.log('Prueba no exitosa') }
+if (resuldado === esperado) { 
+	console.log('Prueba exitosa');
+} else { 
+	throw new Error('Prueba no exitosa');
+	console.error('Prueba no exitosa');
+}
+
+function expect(actual) {
+	
+	return {
+		toBe(expect) {
+			if (actual !== expect) {
+				throw new Error('Prueba no exitosa');
+			}
+		}
+	}
+}
+
+function it(title, callback) {
+	try {
+		callback();
+		console.log(`✔️ ${title}`)
+	} catch(error) {
+		console.error(`✖️ ${title}`)
+	}
+}
+
+it('Funcion saluda', ()=> {
+	expect(saludar('Armando')).toBe('Hola Armando');
+});
