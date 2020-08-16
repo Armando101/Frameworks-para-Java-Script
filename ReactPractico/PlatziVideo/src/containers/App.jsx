913368604349c54df23d303088fdfe4d287054ce
Armando <rivera.armando997@gmail.com>
2020-08-16 03:11:24 -0500

    React Practico - Coustumer hooks

--- ReactPractico/PlatziVideo/src/containers/App.jsx
+++ ReactPractico/PlatziVideo/src/containers/App.jsx
@@ -5,27 +5,30 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initalState';
+
 const App = () => {
-	const [ videos, setVideos ] = useState({ mylist:[], trends:[], originals: []}); 
 
-	useEffect(() => {
-		fetch('http://localhost:3000/initalState')
-		.then(response => response.json())
-		.then(data => setVideos(data));
-	}, []);
+	const initialState = useInitialState(API);
 
-	return (
+	console.log(initialState);
+
+	return initialState.length === 0 ? <h1>Loading...</h1> : (
 		<div className="App">
 			<Header/>
 			<Search/>
 
-			{videos.mylist.length > 0 && 
+			{initialState.mylist.length > 0 && 
 
             <Categories title="Mi lista">
                 <Carousel>
-                    <CarouselItem/>                                                                      
+                    {
+						initialState.mylist.map(item => <CarouselItem key={item.id} {...item}/>)
+					}             
                 </Carousel>
             </Categories>
             }
@@ -33,7 +36,15 @@ const App = () => {
 			<Categories title="Tendencias">
 				<Carousel>
 					{
-						videos.trends.map(item => <CarouselItem key={item.id} {...item}/>)
+						initialState.trends.map(item => <CarouselItem key={item.id} {...item}/>)
+					}
+				</Carousel>
+			</Categories>
+
+			<Categories title="Originales">
+				<Carousel>
+					{
+						initialState.originals.map(item => <CarouselItem key={item.id} {...item}/>)
 					}
 				</Carousel>
 			</Categories>
