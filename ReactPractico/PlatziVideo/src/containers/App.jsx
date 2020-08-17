fbd6629b19a6c4c6d694dd4e48a4ff334c895cb1
Armando <rivera.armando997@gmail.com>
2020-08-16 23:08:27 -0500

    React Practico -Optimizando los carruseles

--- ReactPractico/PlatziVideo/src/containers/App.jsx
+++ ReactPractico/PlatziVideo/src/containers/App.jsx
@@ -15,40 +15,26 @@ const App = () => {
 
 	const initialState = useInitialState(API);
 
-	console.log(initialState);
+	const keys = Object.keys(initialState);
 
 	return initialState.length === 0 ? <h1>Loading...</h1> : (
 		<div className="App">
 			<Header/>
 			<Search/>
 
-			{initialState.mylist.length > 0 && 
+			{
 
-            <Categories title="Mi lista">
+				keys.map(key => {
+					return initialState[key].length > 0 && ( 
+						<Categories title={key}>
                 <Carousel>
-                    {
-						initialState.mylist.map(item => <CarouselItem key={item.id} {...item}/>)
-					}             
+										{
+                    initialState[key].map(item => <CarouselItem key={item.id} {...item}/>)}             
                 </Carousel>
             </Categories>
-            }
-
-			<Categories title="Tendencias">
-				<Carousel>
-					{
-						initialState.trends.map(item => <CarouselItem key={item.id} {...item}/>)
-					}
-				</Carousel>
-			</Categories>
-
-			<Categories title="Originales">
-				<Carousel>
-					{
-						initialState.originals.map(item => <CarouselItem key={item.id} {...item}/>)
-					}
-				</Carousel>
-			</Categories>
-
+          )		
+				})
+			}
 			<Footer/>
 
 		</div>
