002168588d4a3b90107ee3fd1a45ec946ed8aea3
Armando <rivera.armando997@gmail.com>
2020-08-18 12:25:35 -0500

    Backend con firebase configuracion

---
+++ AngularProjects/gotyBackend/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fir-graphics.firebaseio.com"
+});
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+ export const helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.json({
+   	mensaje: "Hello from Firebase Functions!!!"
+   });
+ });
