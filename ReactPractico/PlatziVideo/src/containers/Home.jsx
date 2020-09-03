952514dca306856d95f74075dd382168d0e270ab
Armando <rivera.armando997@gmail.com>
2020-09-03 12:21:35 -0500

    Primera ruta

---
+++ ReactPractico/PlatziVideo/src/containers/Home.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
+
+import '../assets/styles/App.scss';
+
+const API = 'http://localhost:3000/initalState';
+
+const Home = () => {
+  const initialState = useInitialState(API);
+
+  const keys = Object.keys(initialState);
+
+  return initialState.length === 0 ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className='App'>
+      <Header />
+      <Search />
+
+      {keys.map((key, index) => {
+        return (
+          initialState[key].length > 0 && (
+            // eslint-disable-next-line react/no-array-index-key
+            <Categories key={index} title={key}>
+              <Carousel>
+                {initialState[key].map((item) => (
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  <CarouselItem key={item.id} {...item} />
+                ))}
+              </Carousel>
+            </Categories>
+          )
+        );
+      })}
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
