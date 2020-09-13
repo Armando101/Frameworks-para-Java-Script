05be4e4f0bbf908864d555deb35ececf7561d3f2
Armando <rivera.armando997@gmail.com>
2020-09-12 23:54:26 -0500

    Agregando cors

--- AngularSockets/01-server/index.ts
+++ AngularSockets/01-server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import Server from "./classes/server";
 import router from "./routes/router";
+import cors from 'cors';
 
 const server = new Server();
 
@@ -8,8 +9,12 @@ const server = new Server();
 server.app.use(express.urlencoded({ extended: true}));
 server.app.use(express.json());
 
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+// Rutas de servicios
 server.app.use('/', router)
 
 server.start(() => {
     console.log(`Server running at http://localhost:${server.port}`)
-})
+});
