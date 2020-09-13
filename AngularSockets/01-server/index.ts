70c7dfe480a1eae06a121979a52c264eb2059ceb
Armando <rivera.armando997@gmail.com>
2020-09-12 23:49:18 -0500

    Obteniendo informacion con POST

--- AngularSockets/01-server/index.ts
+++ AngularSockets/01-server/index.ts
@@ -1,7 +1,12 @@
+import express from 'express';
 import Server from "./classes/server";
 import router from "./routes/router";
 
 const server = new Server();
+
+// Body parser
+server.app.use(express.urlencoded({ extended: true}));
+server.app.use(express.json());
 
 server.app.use('/', router)
 
--- AngularSockets/01-server/routes/router.ts
+++ AngularSockets/01-server/routes/router.ts
@@ -3,16 +3,23 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/messages', (req: Request, res: Response) => {
+
     res.json({
         ok: true,
-        message: 'Everthing is ok'
+        message: 'Everything is ok'
     });
 });
 
-router.post('/messages', (req: Request, res: Response) => {
+router.post('/messages/:id', (req: Request, res: Response) => {
+
+    const title = req.body.title;
+    const message = req.body.message;
+    const id = req.params.id;
+
     res.json({
         ok: true,
-        message: 'POST ok'
+        title,
+        message
     });
 });
 
