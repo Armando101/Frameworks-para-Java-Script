589e8ece03b9cfef2202e5fc2e3e2aa83250f18e
Armando <rivera.armando997@gmail.com>
2020-09-15 02:16:31 -0500

    Angular Sockets Interfaz visual para ver los mensajes

--- AngularSockets/02-basic/src/app/components/chat/chat.component.ts
+++ AngularSockets/02-basic/src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
 import { Subscription } from 'rxjs';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-chat',
@@ -11,13 +12,23 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   public text = '';
   public messageSubscription: Subscription;
+  public element: HTMLElement;
+  public messages: any[] = [];
 
   constructor(
     private chat: ChatService
   ) { }
 
   ngOnInit(): void {
-    this.messageSubscription = this.chat.getMessages().subscribe(console.log);
+    this.element = document.getElementById('chat-message');
+    this.messageSubscription = this.chat.getMessages().subscribe(msg => {
+      this.messages.push(msg);
+
+      setTimeout(() => {
+        this.element.scrollTop = this.element.scrollHeight;
+      }, 50);
+
+    });
   }
 
   ngOnDestroy(): void {
@@ -25,7 +36,8 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   send(): void {
-    this.chat.sendMessage(this.text);
+    // tslint:disable-next-line: no-unused-expression
+    this.text.trim().length === 0 || this.chat.sendMessage(this.text);
     this.text = '';
   }
 
