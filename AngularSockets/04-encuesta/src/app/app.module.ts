2e7a9f15a1f2a4d8b86bab6933e04a5174e6414b
Armando <rivera.armando997@gmail.com>
2020-09-19 20:52:46 -0500

    Angular sockets Creando componente de grafica con datos quemados y servicio de comunicacion

--- AngularSockets/04-encuesta/src/app/app.module.ts
+++ AngularSockets/04-encuesta/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { GraficaComponent } from './components/grafica/grafica.component';
+import { ChartsModule } from 'ng2-charts';
 
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraficaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ChartsModule,
     SocketIoModule.forRoot(config)
   ],
   providers: [],
---
+++ AngularSockets/04-encuesta/src/app/components/grafica/grafica.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-grafica',
+  templateUrl: './grafica.component.html',
+  styleUrls: ['./grafica.component.css']
+})
+export class GraficaComponent implements OnInit {
+
+  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
