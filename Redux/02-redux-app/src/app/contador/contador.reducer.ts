1a6d017947ac3b8701f4c78f05ea33e9d4578e65
Armando <rivera.armando997@gmail.com>
2020-11-05 19:43:00 -0600

    Redux -Cambiando switch por la funcion recomendada

--- Redux/02-redux-app/src/app/contador/contador.reducer.ts
+++ Redux/02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,15 +1,30 @@
-import { Action } from '@ngrx/store';
+import { Action, createReducer, on } from '@ngrx/store';
 import { increment, decrement } from './contador.actions';
 
-export function contadorReducer(state: number = 10, action: Action): number {
-    switch (action.type) {
-        case increment.type:
-            return state + 1;
+// export function contadorReducer(state: number = 10, action: Action): number {
+//     switch (action.type) {
+//         case increment.type:
+//             return state + 1;
 
-        case decrement.type:
-            return state - 1;
+//         case decrement.type:
+//             return state - 1;
 
-        default:
-            return state;
-    }
+//         default:
+//             return state;
+//     }
+// }
+
+
+// La siguiente manera funciona igual que la anterior solo que esta es la recomendada
+
+export const initialState = 0;
+
+// tslint:disable-next-line: variable-name
+const _countadorReducer = createReducer(initialState,
+    on(increment, (state) => state + 1),
+    on(decrement, (state) => state - 1)
+);
+
+export function contadorReducer(state: any, action: Action): number {
+    return _countadorReducer(state, action);
 }
