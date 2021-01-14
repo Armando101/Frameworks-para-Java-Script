0acdf96c211878b9e66a48b5e022e751bb1925ed
Armano Rivera <rivera.armando997@gmail.com>
2021-01-13 21:37:16 -0600

    Angular Redux; Create reducer

--- Redux/02-redux-app/src/app/contador/contador.reducer.ts
+++ Redux/02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,15 +1,29 @@
-import { Action } from "@ngrx/store";
+import { Action, createReducer, on } from "@ngrx/store";
 import { decrement, increment } from "./contador.actions";
 
-export function contadorReducer(state: number = 10, action: Action) {
+export const initialState = 20;
 
-  switch (action.type) {
-    case increment.type:
-      return state + 1;
-    case decrement.type:
-      return state - 1;
-    default:
-      return state;
-  }
+// export function contadorReducer(state: number = 10, action: Action) {
 
+//   switch (action.type) {
+//     case increment.type:
+//       return state + 1;
+//     case decrement.type:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+
+// }
+
+// Otra manera más eficiente de hacer lo que hicimos con el switch
+
+const _contadorReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1),
+);
+
+export function contadorReducer(state, action) {
+  return _contadorReducer(state, action);
 }
