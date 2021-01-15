131aa0642858028f806ecd2d1e4a6009cce561a3
Armano Rivera <rivera.armando997@gmail.com>
2021-01-15 12:01:53 -0600

    AngularRedux; ToDoApp -Editar una tarea

--- Redux/03-todo-app/src/app/todo/todo-item/todo-item.component.ts
+++ Redux/03-todo-app/src/app/todo/todo-item/todo-item.component.ts
@@ -3,7 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducer';
 import { Todo } from '../models/todo.model';
-import { toggle } from '../todo.actions';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -27,13 +27,14 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
     this.chkCompletado = new FormControl(this.todo.completado);
     this.txtInput = new FormControl(this.todo.texto, Validators.required);
-    this.chkCompletado.valueChanges.subscribe(value => {
-      this.store.dispatch(toggle({ id: this.todo.id }));
+    this.chkCompletado.valueChanges.subscribe(_ => {
+      this.store.dispatch(actions.toggle({ id: this.todo.id }));
     });
   }
 
   editar() {
     this.editing = true;
+    this.txtInput.setValue(this.todo.texto);
     setTimeout(() => {
       this.txtInputFisico.nativeElement.select();
     }, 100);
@@ -41,6 +42,12 @@ export class TodoItemComponent implements OnInit {
 
   terminarEdicion() {
     this.editing = false;
+    if (this.txtInput.invalid || this.txtInput.value === this.todo.texto) return;
+
+    this.store.dispatch(actions.editar({
+      id: this.todo.id,
+      texto: this.txtInput.value
+    }));
   }
 
 }
