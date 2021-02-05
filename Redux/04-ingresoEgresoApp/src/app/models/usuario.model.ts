17905905aa2de8db0dc35376529e4037acd5bb8d
Armando Rivera <rivera.armando997@gmail.com>
2021-02-05 11:49:42 -0600

    01-Redux_IngresoEgreso; Guardar usuarios en firebaseStore

---
+++ Redux/04-ingresoEgresoApp/src/app/models/usuario.model.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+  constructor(
+    public uid: string,
+    public name: string,
+    public email: string,
+  ) {
+
+  }
+}
--- Redux/04-ingresoEgresoApp/src/app/services/auth.service.ts
+++ Redux/04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from "rxjs/operators";
+import { Usuario } from '../models/usuario.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(
+    public auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) { }
 
   initAuthListener() {
     this.auth.authState.subscribe(fuser => {
@@ -15,7 +20,12 @@ export class AuthService {
   }
 
   crearUsuario(name: string, email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new Usuario(user.uid, name, email);
+        return this.firestore.doc(`${user.uid}/usuario`)
+          .set({ ...newUser });
+      });
   }
 
   login(email: string, password: string) {
@@ -24,6 +34,12 @@ export class AuthService {
 
   logout() {
     return this.auth.signOut();
+  }
+
+  isAuth() {
+    return this.auth.authState.pipe(
+      map(fbUser => fbUser != null)
+    );
   }
 
 }
