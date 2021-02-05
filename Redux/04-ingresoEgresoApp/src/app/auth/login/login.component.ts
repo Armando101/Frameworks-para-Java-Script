4ea037b9e65c15f583d6b2d059ad8d6ee2c4ed95
Armando Rivera <rivera.armando997@gmail.com>
2021-02-04 18:59:19 -0600

    01-Redux_IngresosEgress; sweet alerts

--- Redux/04-ingresoEgresoApp/src/app/auth/login/login.component.ts
+++ Redux/04-ingresoEgresoApp/src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +14,34 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({});
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
   login() {
-
+    if (this.loginForm.invalid) return;
+    const { email, password } = this.loginForm.value;
+    this.auth.login(email, password)
+      .then(credentials => {
+        console.log(credentials);
+        this.router.navigate(['/'])
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message,
+        });
+      });
   }
 
 }
