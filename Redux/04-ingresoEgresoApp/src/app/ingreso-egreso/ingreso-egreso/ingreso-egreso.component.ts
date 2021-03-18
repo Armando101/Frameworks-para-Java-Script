8049f0592fa577b8e1a983640e56e8ef27d8b569
Armando Rivera <rivera.armando997@gmail.com>
2021-03-18 13:03:57 -0600

    01_Redux; 03_Loading para agregar usuarios

--- Redux/04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso/ingreso-egreso.component.ts
+++ Redux/04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
 import { IngresoEgresoService } from 'src/app/services/ingreso-egreso.service';
+import * as ui from 'src/app/shared/ui.actions';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,13 +13,16 @@ import Swal from 'sweetalert2';
   templateUrl: './ingreso-egreso.component.html',
   styles: [],
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
   ingresoForm: FormGroup;
   tipo: string = 'ingreso';
+  loading: boolean = false;
+  subscriptionLoading: Subscription;
 
   constructor(
     private fb: FormBuilder,
-    private ingresoEgresoService: IngresoEgresoService
+    private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>
   ) {}
 
   ngOnInit(): void {
@@ -23,9 +30,13 @@ export class IngresoEgresoComponent implements OnInit {
       descripcion: ['', Validators.required],
       monto: ['', Validators.required],
     });
+    this.subscriptionLoading = this.store.select('ui').subscribe((ui) => {
+      this.loading = ui.isLoading;
+    });
   }
 
   guardar() {
+    this.store.dispatch(ui.isLoading());
     if (this.ingresoForm.invalid) {
       return;
     }
@@ -39,9 +50,15 @@ export class IngresoEgresoComponent implements OnInit {
       .then(() => {
         this.ingresoForm.reset();
         Swal.fire('Registro creado', descripcion, 'success');
+        this.store.dispatch(ui.stopLoading());
       })
       .catch((err) => {
         Swal.fire('Error!!', err.message, 'error');
+        this.store.dispatch(ui.stopLoading());
       });
+  }
+
+  ngOnDestroy() {
+    this.subscriptionLoading.unsubscribe();
   }
 }
